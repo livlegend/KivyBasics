d4cb85d6fd822ecebdd87958ee0ff2c8cdbbc6c9
livlegend <livsenyo@gmail.com>
2021-03-18 06:56:55 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from kivymd.app import MDApp
+
+class DemoApp (MDApp):
+    def build(self):
+        return
+
+DemoApp().run()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
