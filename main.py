46dee909ae9856f36721f06e014ac26057c16a6c
livlegend <livsenyo@gmail.com>
2021-03-25 06:43:44 +0000

    labels and icons

--- main.py
+++ main.py
@@ -3,10 +3,12 @@
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from kivymd.app import MDApp
-
+from kivymd.uix.label import MDLabel,MDIcon
 class DemoApp (MDApp):
     def build(self):
-        return
+        label=MDLabel(text='Hello word', halign='center', theme_text_color='Secondary',font_style='H1')
+        icon_label=MDIcon(icon='language-python')
+        return icon_label
 
 DemoApp().run()
 
