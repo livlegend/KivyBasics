c557102d066c69dbc1e37f42f5955d6dd93678c1
livlegend <livsenyo@gmail.com>
2021-03-26 06:41:17 +0000

    theming

--- main.py
+++ main.py
@@ -3,12 +3,20 @@
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from kivymd.app import MDApp
+from kivy.uix.screenmanager import Screen
 from kivymd.uix.label import MDLabel,MDIcon
+from kivymd.uix.button import MDRectangleFlatButton
+
 class DemoApp (MDApp):
     def build(self):
-        label=MDLabel(text='Hello word', halign='center', theme_text_color='Secondary',font_style='H1')
-        icon_label=MDIcon(icon='language-python')
-        return icon_label
+        self.theme_cls.primary_palette="Blue"
+        self.theme_cls.primary_hue = "200"
+        screen = Screen()
+        screen.add_widget(
+            MDRectangleFlatButton(text='Hello People',
+                                  pos_hint={"center_x":0.5,"center_y":0.5})
+        )
+        return screen
 
 DemoApp().run()
 
