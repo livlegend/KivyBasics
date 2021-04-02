a4716117e3c028641ca6647b7950eed26f3725bb
livlegend <livsenyo@gmail.com>
2021-04-02 06:39:51 +0000

    toolvar

--- main.py
+++ main.py
@@ -2,21 +2,66 @@
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from kivy.lang import Builder
+
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import Screen
-from kivymd.uix.label import MDLabel,MDIcon
-from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.taptargetview import MDTapTargetView
+
+KV = '''
+MDScreen:
+    MDBoxLayout:
+        orientation: "vertical"
+
+        MDToolbar:
+            title: "Mon toolbar"
+            left_action_items: [["menu", lambda x: app.callback()]]
+        
+        
+        MDCard:
+            orientation: "vertical"
+            padding: "8dp"
+            size_hint: None, None
+            size: "280dp", "180dp"
+            halign: "center"
+            pos_hint: {"center_x": .5, "center_y": .5}
+    
+            MDLabel:
+                text: "Test de lecture"
+                theme_text_color: "Secondary"
+                size_hint_y: None
+                height: self.texture_size[1]
+    
+            MDSeparator:
+                height: "1dp"
+    
+            MDLabel:
+                text: "Body"
+                
+        MDFloatingActionButton:
+            id: button
+            icon: "plus"
+            pos: 10, 10
+            on_release: app.tap_target_start()
+            
+'''
 
 class DemoApp (MDApp):
     def build(self):
-        self.theme_cls.primary_palette="Blue"
-        self.theme_cls.primary_hue = "200"
-        screen = Screen()
-        screen.add_widget(
-            MDRectangleFlatButton(text='Hello People',
-                                  pos_hint={"center_x":0.5,"center_y":0.5})
+        screen=Builder.load_string(KV)
+        self.tap_target_view = MDTapTargetView(
+            widget=screen.ids.button,
+            title_text="Lire",
+            description_text="Patientez en écoutant la lecture de ce texte",
+            widget_position="left_bottom",
         )
         return screen
+
+
+    def tap_target_start(self):
+        if self.tap_target_view.state == "close":
+            self.tap_target_view.start()
+        else:
+            self.tap_target_view.stop()
 
 DemoApp().run()
 
